3cf73053fa05310bc7083027bccff7e890f142cd
furkangokk <furkanngok0@gmail.com>
2026-07-17 18:04:32 +0300

    feat: Add treasure chest with interaction system

---
+++ Source/ActionRoguelite/Public/SItemChest.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SGameplayInterface.h"
+#include "SItemChest.generated.h"
+  
+UCLASS()
+class ACTIONROGUELITE_API ASItemChest : public AActor, public ISGameplayInterface
+{
+	GENERATED_BODY()
+
+	void Interact_Implementatiton(APawn* IntigatorPawn);
+	
+protected:
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* BaseMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* LidMesh;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	ASItemChest();
+
+};
