2d80ce94eb4cf5fca83b38285413e018aba6e87d
furkangokk <furkanngok0@gmail.com>
2026-07-26 20:03:01 +0300

    feat: Magic projectile inherited from Projectile Base

--- Source/ActionRoguelite/Private/SMagicProjectile.cpp
+++ Source/ActionRoguelite/Private/SMagicProjectile.cpp
@@ -9,28 +9,6 @@
 // Sets default values
 ASMagicProjectile::ASMagicProjectile()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
-	SphereComp = CreateDefaultSubobject<USphereComponent>("SphereComp");
-	SphereComp -> SetCollisionProfileName("Projectile");
-
-	SphereComp->SetNotifyRigidBodyCollision(true);
-
-	RootComponent = SphereComp;
-
-
-
-	EffectComp = CreateDefaultSubobject<UParticleSystemComponent>("EffectComp");
-	EffectComp->SetupAttachment(SphereComp);
-
-	MovementComp = CreateDefaultSubobject<UProjectileMovementComponent>("MovementComp");
-	MovementComp->InitialSpeed = 1000.0f;
-	MovementComp->bRotationFollowsVelocity = true;
-	MovementComp->bInitialVelocityInLocalSpace = true;
-
-
-
 
 }
 
--- Source/ActionRoguelite/Public/SMagicProjectile.h
+++ Source/ActionRoguelite/Public/SMagicProjectile.h
@@ -1,9 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "ProjectileBase.h"
 #include "SMagicProjectile.generated.h"
 
 class USphereComponent;
@@ -11,7 +10,7 @@ class UParticleSystemComponent;
 class UProjectileMovementComponent;
 
 UCLASS()
-class ACTIONROGUELITE_API ASMagicProjectile : public AActor
+class ACTIONROGUELITE_API ASMagicProjectile : public AProjectileBase
 {
 	GENERATED_BODY()
 	
@@ -20,14 +19,6 @@ public:
 	ASMagicProjectile();
 
 protected:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	USphereComponent *SphereComp;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	UParticleSystemComponent *EffectComp;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	UProjectileMovementComponent *MovementComp;
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
