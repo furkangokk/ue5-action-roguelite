8b6fe54b312f90640a3a9d1cce39fa0f9885f8c8
furkangokk <furkanngok0@gmail.com>
2026-08-01 16:51:00 +0300

    feat: Add Health attribute and ApplyHealthChange to SAttributeComponent

--- Source/ActionRoguelite/Private/SAttributeComponent.cpp
+++ Source/ActionRoguelite/Private/SAttributeComponent.cpp
@@ -1,34 +1,20 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "SAttributeComponent.h"
 
-// Sets default values for this component's properties
 USAttributeComponent::USAttributeComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+	Health = 100;
 }
 
 
-// Called when the game starts
-void USAttributeComponent::BeginPlay()
+
+bool USAttributeComponent::ApplyHealthChange(float Delta)
 {
-	Super::BeginPlay();
+	Health += Delta;
 
-	// ...
-	
+	return true;
 }
 
 
-// Called every frame
-void USAttributeComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
-}
 
--- Source/ActionRoguelite/Public/SAttributeComponent.h
+++ Source/ActionRoguelite/Public/SAttributeComponent.h
@@ -17,12 +17,27 @@ public:
 	USAttributeComponent();
 
 protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
+
+	// EditAnywhere - edit in BP editor and per-instance in level.
+	// VisibleAnywhere - 'read-only' in editor and level. (Use for Components)
+	// EditDefaultsOnly - hide variable per-instance, edit in BP editor only
+	// VisibleDefaultsOnly - 'read-only' access for variable, only in BP editor (uncommon)
+	// EditInstanceOnly - allow only editing of instance (eg. when placed in level)
+	// --
+	// BlueprintReadOnly - read-only in the Blueprint scripting (does not affect 'details'-panel)
+	// BlueprintReadWrite - read-write access in Blueprints
+	// --
+	// Category = "" - display only for detail panels and blueprint context menu.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attributes")
+	float Health;
+
+
 
 public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	UFUNCTION(BlueprintCallable, Category = "Attributes")
+	bool ApplyHealthChange(float Delta);
+
 
 		
 };
+	
