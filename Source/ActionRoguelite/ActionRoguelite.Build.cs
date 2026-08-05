b18ee76f49dedd5175e23b4356d769d6eb9170b7
furkangokk <furkanngok0@gmail.com>
2026-08-05 18:41:49 +0300

    feat: Add AI minion with controller and attack-range BT service

--- Source/ActionRoguelite/ActionRoguelite.Build.cs
+++ Source/ActionRoguelite/ActionRoguelite.Build.cs
@@ -8,7 +8,7 @@ public class ActionRoguelite : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput","Niagara" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput","Niagara","AIModule","GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
